2767265c73bebc131d5c1fa866084a4fa600c700
NMRUDULA25 <narayanammrudula6@gmail.com>
2025-07-02 14:54:43 +0530

    Add files via upload

---
+++ funexp.js
@@ -0,0 +1,15 @@
+function welcomeMessage(){
+    console.log("Welcome To Our College");
+}
+welcomeMessage();
+
+function getMessage(a,b){
+    console.log(a);
+    console.log(b);
+    return a+b;
+}
+console.log(getMessage(10,20));
+const multiply=(a,b)=>(a*b);
+console.log(multiply(13,12));
+const greet=()=>("HELLO");
+console.log(greet());
